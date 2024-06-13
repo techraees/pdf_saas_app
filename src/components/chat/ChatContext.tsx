49c77b8fc672e95b33e8787267fd7c67f48d1a52
techraees <techraees786@gmail.com>
2024-06-13 10:57:57 +0500

    Convert into js next.js app.

--- src/components/chat/ChatContext.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ChatContext = () => {
-  return <div></div>;
-};
-
-export default ChatContext;
--- src/components/ui/card.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const card = () => {
-  return <div></div>;
-};
-
-export default card;
