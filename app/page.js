b44a7676fa8ebbf395191b1e33d184f5d81ff333
techraees <techraees786@gmail.com>
2024-06-23 03:11:48 +0500

    Change the name of website.

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
           <p className="text-sm font-semibold text-gray-700">
-            Quill is now public!
+            Rpdf is now public!
           </p>
         </div>
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
@@ -20,7 +20,7 @@ export default function Home() {
           seconds.
         </h1>
         <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-          Quill allows you to have conversations with any PDF document. Simply
+          Rpdf allows you to have conversations with any PDF document. Simply
           upload your file and start asking questions right away.
         </p>
 
@@ -92,7 +92,7 @@ export default function Home() {
               Start chatting in minutes
             </h2>
             <p className="mt-4 text-lg text-gray-600">
-              Chatting to your PDF files has never been easier than with Quill.
+              Chatting to your PDF files has never been easier than with Rpdf.
             </p>
           </div>
         </div>
@@ -136,7 +136,7 @@ export default function Home() {
                 Start asking questions
               </span>
               <span className="mt-2 text-zinc-700">
-                It&apos;s that simple. Try out Quill today - it really takes
+                It&apos;s that simple. Try out Rpdf today - it really takes
                 less than a minute.
               </span>
             </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href="/" className="flex z-40 font-semibold">
-            <span>quill.</span>
+            <span>rpdf.</span>
           </Link>
 
           <MobileNav isAuth={!!user} />
